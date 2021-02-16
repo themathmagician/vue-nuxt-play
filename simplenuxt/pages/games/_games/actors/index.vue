c90a3b888728349421c00b169b7ecc3052f0d979
themathmagician <agata@mathmagicians.dk>
2021-02-16 18:17:49 +0100

    ITYS-79 #comment Actor index working. TODO update ui for actor card

--- simplenuxt/pages/games/_games/actors/index.vue
+++ simplenuxt/pages/games/_games/actors/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>All Actors in {{this.game}}</h1>
+    <card-title>All Actors in {{this.game}}</card-title>
     <ActorCard v-for="ac in $store.getters.actorNames"  :actor="ac" :name="ac" :key="ac"/>
   </div>
 </template>
@@ -13,7 +13,7 @@ export default {
   async asyncData({store, params}) {
     //TODO add parameter here
     const game = params.games
-    await store.dispatch( 'fetchActorsForGame', games)
+    await store.dispatch( 'fetchActorsForGame', game)
     return {game}
   }
 }
